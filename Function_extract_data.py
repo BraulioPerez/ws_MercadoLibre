98eb1b2df3dff9af11ab2088fa4f27a49190b56b
Frank3040 <wxyz4150@gmail.com>
2024-02-01 17:22:17 -0600

    Fixed some bugs of my code

--- Function_extract_data.py
+++ Function_extract_data.py
@@ -14,42 +14,42 @@ def get_information(url:str):
     
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
-    
-    
-    title = soup.find("h1", {"class": "ui-pdp-title"}).text
-    prices = soup.find_all("span", {"class": "andes-money-amount__fraction"}, limit=2)
-
-
-    desc = soup.find("p", {"class": "ui-pdp-description__content"}).text
-    seller = soup.find("div", {"class":"ui-pdp-seller__header__title"})
-    complete_sellername = seller.find_all("span")
-    if len(complete_sellername) == 2:
-        seller_name = complete_sellername[0].text + " " + complete_sellername[1].text
-    else:
-        seller_name = complete_sellername.text
 
     #rate = soup.find("span", attrs={"aria-hidden":"true", "class":"ui-pdp-review__rating"})
     #amont_rates = soup.find("span", attrs={"aria-hidden":"true", "class":"ui-pdp-review__amount"})
     try:
+        title = soup.find("h1", {"class": "ui-pdp-title"}).text
+        prices = soup.find_all("span", {"class": "andes-money-amount__fraction"}, limit=2)
+
+
+        desc = soup.find("p", {"class": "ui-pdp-description__content"}).text
+        seller = soup.find("div", {"class":"ui-pdp-seller__header__title"})
+        complete_sellername = seller.find_all("span")
+        if len(complete_sellername) == 2:
+            seller_name = complete_sellername[0].text + " " + complete_sellername[1].text
+        else:
+            seller_name = complete_sellername.text
+
         rate_div = soup.find("div", attrs={"class":"ui-review-capability__rating"})
         rate_p = rate_div.find_all("p")
         rate = rate_p[0].text
         amont_rates = rate_p[2].text
+        
+        store_url_div = soup.find("div", {"class":"ui-pdp-container__col col-2 mr-32"})
+        store_url = store_url_div.find("a")['href']
+        
+        if len(prices) == 2:
+            price_before = prices[0].text
+            price_current = prices[1].text
+            information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]
+        else:
+            price_current = prices[0].text
+            price_before = None
+            information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]
+            
     except Exception as e:
        # By this way we can know about the type of error occurring
         print("The error is: ",e)
-
-    store_url_div = soup.find("div", {"class":"ui-pdp-container__col col-2 mr-32"})
-    store_url = store_url_div.find("a")['href']
-    
-    if len(prices) == 2:
-        price_before = prices[0].text
-        price_current = prices[1].text
-        information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]
-    else:
-        price_current = prices[0].text
-        price_before = None
-        information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]
 
     return information_list
     
