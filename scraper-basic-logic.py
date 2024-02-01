39b0a9551d89136af7176d41b2546ed36c154cf5
Braulio Perez <braulioperez.contacto@gmail.com>
2024-02-01 17:49:21 -0600

    intentando hacer que funcione extraccion de elementos

--- scraper-basic-logic.py
+++ scraper-basic-logic.py
@@ -22,53 +22,73 @@ def extract_href(session, url):
 
 
 def get_information(url:str):
-    try:
-        #Generar soup
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text, "html.parser")
+    return None
+    # try:
+    #     #Generar soup
+    #     res = requests.get(url)
+    #     soup = BeautifulSoup(res.text, "html.parser")
         
-        #Obtener titulo
-        title = soup.find("h1", {"class": "ui-pdp-title"}).text
-        prices = soup.find_all("span", {"class": "andes-money-amount__fraction"}, limit=2)
+    #     #Obtener titulo
+    #     title = soup.find("h1", {"class": "ui-pdp-title"}).text
+    #     prices = soup.find_all("span", {"class": "andes-money-amount__fraction"}, limit=2)
 
-        #Obtener precios
-        desc = soup.find("p", {"class": "ui-pdp-description__content"}).text
+    #     #Obtener precios
+    #     desc = soup.find("p", {"class": "ui-pdp-description__content"}).text
         
-        #Obtener vendedor
-        seller = soup.find("div", {"class":"ui-pdp-seller__header__title"})
-        complete_sellername = seller.find_all("span")
+    #     #Obtener vendedor
+    #     seller = soup.find("div", {"class":"ui-pdp-seller__header__title"})
+    #     complete_sellername = seller.find_all("span")
         
-        #Dividir entre nombre del vendedor y separar link
-        if len(complete_sellername) == 2:
-            seller_name = complete_sellername[0].text + " " + complete_sellername[1].text
-        else:
-            seller_name = complete_sellername.text
+    #     #Dividir entre nombre del vendedor y separar link
+    #     if len(complete_sellername) == 2:
+    #         seller_name = complete_sellername[0].text + " " + complete_sellername[1].text
+    #     else:
+    #         seller_name = complete_sellername.text
 
-        rate_div = soup.find("div", attrs={"class":"ui-review-capability__rating"})
-        rate_p = rate_div.find_all("p")
-        rate = rate_p[0].text
-        amont_rates = rate_p[2].text
+    #     rate_div = soup.find("div", attrs={"class":"ui-review-capability__rating"})
+    #     rate_p = rate_div.find_all("p")
+    #     rate = rate_p[0].text
+    #     amont_rates = rate_p[2].text
 
-        store_url_div = soup.find("div", {"class":"ui-pdp-container__col col-2 mr-32"})
-        store_url = store_url_div.find("a")['href']
+    #     store_url_div = soup.find("div", {"class":"ui-pdp-container__col col-2 mr-32"})
+    #     store_url = store_url_div.find("a")['href']
     
-        if len(prices) == 2:
-            price_before = prices[0].text
-            price_current = prices[1].text
-            information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]            
-        else:
-            price_current = prices[0].text          
-            price_before = None         
-            information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]            
+    #     if len(prices) == 2:
+    #         price_before = prices[0].text
+    #         price_current = prices[1].text
+    #         information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]            
+    #     else:
+    #         price_current = prices[0].text          
+    #         price_before = None         
+    #         information_list = [title, seller_name, store_url, price_before, price_current, desc, rate, amont_rates]            
 
-        return information_list
-
-    except Exception as e:
-       # By this way we can know about the type of error occurring
-        print("The error is: ",e)
-        return []
+    #     return information_list
 
 
+
+    # except Exception as e:
+    #    # By this way we can know about the type of error occurring
+    #     print("The error is: ",e)
+    #     return []
+
+
+def extraer_elementos(url):
+    # Realizar la solicitud HTTP a la url
+    response = requests.get(url)
+    
+    # Crear una instancia de BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Buscar los elementos por clase y extraer el texto
+    titulo = soup.find(class_='ui-pdp-title').get_text()
+    fraccion = soup.find(class_='andes-money-amount__fraction').get_text()
+    contenido = soup.find(class_='ui-pdp-description__content').get_text()
+    vendedor = soup.find(class_='ui-pdp-seller__header__title').get_text()
+    
+    # Devolver los resultados en un diccionario
+    resultados = [titulo, fraccion, contenido, vendedor]
+    print(resultados)
+    return resultados
 
 
 def increase_page_num(url: str):
@@ -112,6 +132,7 @@ if __name__ == '__main__':
     #se aplican las funciones con la funcion map para que sea más rápido y evitar loops sobre loops sobre loops
     urls = list(map(create_search_link, items))
     all_urls = list(map(extract_item_urls, urls))
-    all_item_info = list(map(get_information, all_urls))
+    for url in all_urls:
+        extraer_elementos(url)
     end = time.time()
-    print(f"el programa tardó {(end-start)} minutos")
+    print(f"el programa tardó {(end-start)//60} minutos")
