720a23d205ce26602adf14242b061e4b685bf06b
Braulio Perez <braulioperez.contacto@gmail.com>
2024-02-26 21:56:27 -0600

    segunda entrega lista en develop

--- item_data_scraper.py
+++ item_data_scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import json
 import os
 import time
-import re
 
 
 
@@ -14,19 +13,15 @@ class ItemDataScraper:
         start = time.time()
         with requests.Session() as session:
             self.session = session.get(self.url)
-        print(f"tiempo de request {(time.time() - start)} seconds")
 
 
     def title_scrap(self):
         # input = url_objeto
         # funcion = extraer titulo de objeto
         # output = string del titulo
-        print("Scraping title")
 
         soup = BeautifulSoup(self.session.text, "html.parser")
         title = soup.select("div.ui-pdp-header__title-container h1")[0].text
-        
-        print(f"Title scraped: {title}")
         return title
 
 
@@ -34,7 +29,6 @@ class ItemDataScraper:
         # input = url_objeto
         # funcion = extraer precio de objeto
         # output = string del precio
-        print("Scraping price")
 
         soup = BeautifulSoup(self.session.text, "html.parser")
         prices = soup.select("div.ui-pdp-price__main-container span.andes-money-amount__fraction")
@@ -46,15 +40,12 @@ class ItemDataScraper:
                 price_current = price_before
                 price_before = None
             prices_list = [price_before, price_current]
-            print(f"Prices scraped {prices_list}")
         elif len(prices) ==0:
-            prices_list = None
-            print(f"Prices scraped {prices_list}")            
+            prices_list = None          
         else:
             price_current = prices[0].text
             price_before = None
             prices_list = [price_before, price_current]
-            print(f"Prices scraped {prices_list}")
             
         return prices_list
     
@@ -62,7 +53,6 @@ class ItemDataScraper:
         # input = url_objeto
         # funcion = extraer rating de objeto
         # output = string del rating
-        print("Scraping rating")
 
         soup = BeautifulSoup(self.session.text, "html.parser")
         review_div = soup.select("div[class*='ui-review-capability__rating'] p")
@@ -73,7 +63,6 @@ class ItemDataScraper:
         else:
             rate = None
 
-        print(f"Rating scraped {rate}")
         return rate
 
 
@@ -81,7 +70,6 @@ class ItemDataScraper:
         # input = url_objeto
         # funcion = extraer url de imagenes de objeto
         # output = lista de urls
-        print("Scraping images")
 
         soup = BeautifulSoup(self.session.text, "html.parser")
         images_list = soup.select("div.ui-pdp-gallery__column span.ui-pdp-gallery__wrapper label.ui-pdp-gallery__label div[role='presentation'] div.ui-pdp-thumbnail__picture img.ui-pdp-image")
@@ -90,7 +78,6 @@ class ItemDataScraper:
             url = img.get('data-src')
             urls_img.append(url)   
         
-        print(f"Images scraped: {urls_img}")
         return urls_img
     
     
@@ -98,7 +85,6 @@ class ItemDataScraper:
     # input = url_objeto
     # funcion = extraer titulo de proveedor del objeto
     # output = string del titulo del proveedor
-        print("Scraping provider")
 
         try:
             soup = BeautifulSoup(self.session.text, "html.parser")
@@ -109,7 +95,6 @@ class ItemDataScraper:
             else:
                 provider = provider_list[0].text 
 
-            print(f"Provider scraped {provider}")
             return provider
         except Exception as e:
             print(f"An error occurred: {e}")
@@ -121,7 +106,6 @@ class ItemDataScraper:
         # input = url_objeto
         # funcion = extraer Descripcion de objeto
         # output = string de la descripcion
-        print("Scraping  Description")
 
         soup = BeautifulSoup(self.session.text, "html.parser")        
         description_list = soup.select("div[class*='ui-pdp-container__row'] div.ui-pdp-description p.ui-pdp-description__content")
@@ -130,7 +114,6 @@ class ItemDataScraper:
         else:
             description = description_list[0].text
 
-        print(f"Description scraped {description}")
         return description
 
 
@@ -143,9 +126,6 @@ class ItemDataScraper:
                     "provider": self.provider_scrap(),
                     "description": self.description_scrap() 
                     }
-        # with open(f"{title}.json", "w") as outfile: 
-        #     json.dump(result, outfile)
-        
         return result
 
 
@@ -156,14 +136,3 @@ class ItemDataScraper:
         if not os.path.isfile(file_name):
             with open(file_name, "w", encoding="utf-8") as file:
                 file.write(str(soup))
-    
-# solo se asigna el url a una variable para que sea mas facil de manipular y testear
-# se crea el onjeto
-# #se ejecuta el metodo data_scrap en el objeto calculando el tiempo que tarda y se imprime el tiempo total de ejecucion
-url = "https://articulo.mercadolibre.com.mx/MLM-1328094335-pc-gamer-arcoteck-ryzen-5-5600g-16gb-ssd-480gb-gabinete-rgb-_JM#position=55&search_layout=stack&type=item&tracking_id=c09b0ff0-2931-4c70-85f2-86b3369f719b"
-
-obj = ItemDataScraper(url)
-start = time.time() 
-obj.data_scrap()
-end = time.time()
-print(f"tardó {(end-start)} segundos")
